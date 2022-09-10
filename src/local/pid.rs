7f9cc0de91aaabef04b14651f8e79c6448aec3c8
Austin Graham <austin_graham@intuit.com>
2022-09-10 15:12:14 -0700

    currently running pid fetch

--- src/local/pid.rs
+++ src/local/pid.rs
@@ -1,7 +1,7 @@
 use std::{
     path::PathBuf,
     fs,
-    io
+    io::{self, BufReader, BufRead}, collections::HashSet, str::FromStr, hash::{Hasher, Hash}
 };
 
 use mocktopus::macros::mockable;
@@ -69,6 +69,58 @@ fn get_or_create_pid_file() -> Result<fs::File, String> {
     }
 }
 
+/// Stores an entry in the PIDs file, hashed
+/// by the port number to distinguish between the several 
+/// servers that could be running at a time.
+#[derive(Debug, PartialEq, Eq)]
+pub struct PIDEntry {
+    pub port: String,
+    pub pid: u32
+}
+
+impl Hash for PIDEntry {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.pid.hash(state);
+    }
+}
+
+/// Get a set of the running PIDs and the matching port numbers.
+/// 
+/// # Examples
+/// ```
+/// get_currently_running_pids().expect("Failed to get pids.");
+/// ```
+#[mockable]
+pub fn get_currently_running_pids() -> Result<HashSet<PIDEntry>, String> {
+    match get_or_create_pid_file() {
+        Ok(file_handler) => {
+            let reader = BufReader::new(file_handler);
+            Ok(reader.lines()
+                  .filter(|line| line.is_ok())
+                  .map(|line| {
+                        let line_str = line.unwrap();
+                        let vector_entries: Vec<&str> = line_str.split_whitespace().collect();
+                        if vector_entries.len() != 2 {
+                            Err(())
+                        } else {
+                            let pid_result = <u32 as FromStr>::from_str(vector_entries[1]);
+                            match pid_result {
+                                Ok(pid) => Ok(PIDEntry {
+                                    port: vector_entries[0].to_string(),
+                                    pid: pid
+                                }),
+                                Err(_) => Err(())
+                            }
+                        }
+                  })
+                  .filter(|entry| entry.is_ok())
+                  .map(|entry| entry.unwrap())
+                  .collect::<HashSet<PIDEntry>>())
+        },
+        Err(msg) => Err(msg)
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -121,6 +173,24 @@ mod tests {
         assert!(file_result.is_err());
 
         fs::remove_file(SERVER_PID_FILE_NAME).expect("Failed to remove temporary file.");
+    }
+
+    #[test]
+    fn test_get_currently_running_pids_success() {
+        let test_path = PathBuf::from("./test/tests.pid");
+        get_or_create_pid_file.mock_safe(move || MockResult::Return(Ok(fs::File::open(&test_path).expect("Failed to open test file."))));
+
+        let pids = get_currently_running_pids();
+        assert!(pids.is_ok());
+        assert_eq!(pids.unwrap().len(), 3);
+    }
+
+    #[test]
+    fn test_get_currently_running_pids_failure() {
+        get_or_create_pid_file.mock_safe(|| MockResult::Return(Err("We failed....".to_string())));
+
+        let pids = get_currently_running_pids();
+        assert!(pids.is_err());
     }
 
 }
